ad6dd69755a788a93a596a3709056119c72dcafb
vida <momenzadeh.v@gmail.com>
2019-02-08 19:34:41 +0330

    first commit

---
+++ app/Question.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    //
+    protected $fillable = [
+	    'question',
+	    'category_id',
+	    'type',
+	    'options'
+	];
+}
